e4eb990d6a39f83dde81bb7baf1cda0b03d07cfa
Caio Teixeira <tex.caio@gmail.com>
2021-08-31 22:41:53 -0300

    logs

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+console.log(process.env.MONGO_URI)
+console.log(process.env.SENDGRID_API_KEY)
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Database connection Success.");
+    console.log("Database connection succeed.");
   })
   .catch((err) => {
     console.error("Mongo Connection Error", err);
@@ -30,5 +33,5 @@ app.use("/users", require("./routes/users"));
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-  console.log("Server started listening on PORT : " + port);
+  console.log("Server started listening on PORT: " + port);
 });
